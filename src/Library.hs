2e38bdf0c3a9faedc20b6f263be3e9fa0a3531ac
Augusto Lecaldare <augustolecaldare@gmail.com>
2021-04-26 22:33:52 -0300

    Se completa el ejercicio de Tecnicas de golpes

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,35 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+type Horas = Number
+type Objetivo = String
+type Presion = Number
+type Golpe = Objetivo -> Number
+
+--EJ 1)
+presionGolpe :: Horas -> Objetivo -> Presion
+presionGolpe horas objetivo = poder horas / fortaleza objetivo
+
+poder :: Horas -> Horas -- 15 veces horas de entrenamiento
+poder horas = (15*) horas
+
+fortaleza :: Golpe -- Cantidad del doble de letras de su nombre
+fortaleza = (2*).length
+--EJ 2)
+gomuGomu :: Golpe -- Golpe al cual ya se le dedico 180 horas
+gomuGomu = presionGolpe 180
+
+normalesConsecutivos :: Golpe -- Golpe al cual ya se le dedico 240 horas
+normalesConsecutivos = presionGolpe 240
+--EJ 3)
+esDificil :: Objetivo -> Bool
+esDificil = (<100).gomuGomu
+
+focalizarObjetivo :: Objetivo -> Objetivo
+focalizarObjetivo = take 7 
+
+valorEnRango :: Number -> Bool
+valorEnRango medio = 200 <= medio && medio <= 400
+
+esAccesible :: Objetivo -> Bool
+esAccesible = valorEnRango.normalesConsecutivos.focalizarObjetivo
--- src/Spec.hs
+++ src/Spec.hs
@@ -5,7 +5,23 @@ import Test.Hspec
 
 correrTests :: IO ()
 correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+  describe "Test 1) presionGolpe" $ do
+    it "La presion de un golpe con 200 horas de entrenamiento sobre una Bolsa de Entrenamiento deberia ser aproximadamente de 68" $ do
+      presionGolpe 200 "Bolsa de Entrenamiento" `shouldBe` 68.181818182
+  describe "Test 2.1) Golpe Gomu-Gomu Elephant Gatling" $ do  
+    it "La presion de un golpe Gomo Gomu sobre la Bolsa de Entrenamineto deberia ser aproximadamente de 61" $ do
+      gomuGomu  "Bolsa de Entrenamiento"  `shouldBe` 61.363636364
+  describe "Test 2.2) normalesConsecutivos" $ do  
+    it "La presion de unos Golpes Normales Consecutivos sobre la Bolsa de Entrenamiento deberia ser aproximadamente de 81 " $ do
+      normalesConsecutivos "Bolsa de Entrenamiento"  `shouldBe` 81.818181818
+  describe "Test 3.1) esDificil" $ do  
+    it "La bolsa de entrenamiento es un objetivo Dificil" $ do
+      esDificil "Bolsa de Entrenamiento"  `shouldBe` True
+    it "El puf no es un objetivo dificil" $ do
+     esDificil "Puf" `shouldBe` False
+  describe "Test 3.2) esAccesible" $ do
+    it "Una Bolsa de Entrenamiento es accesible" $ do
+      esAccesible "Bolsa de entranamiento" `shouldBe` True
+    it "Un puf no es accesible" $ do
+      esAccesible "puf" `shouldBe` False 
 
